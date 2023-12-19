8f8271ea56d765fddb7a229585c3fdb7e1a9220f
Thomas Alcala Schneider <thomas@lockrmail.com>
2023-12-19 19:08:19 +0100

    feat: add commands

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # tune.nvim
 
 A simple theme switcher for people that like to mix it up from time to time!
+
+## Installation
+
+### With lazy.nvim
+
+```lua
+{
+  ‘thomasmarcel/tune.nvim’,
+  config = function()
+    require('tune').setup({
+      -- Set up a default colorscheme
+      default_colorscheme = 'rose-pine'
+    })
+  end,
+  priority = 10,
+}
+```
+
+## Usage
+
+tune.nvim provide the following commands:
+- `:Tune <colorscheme>` to select a colorscheme
+- `:TuneRandom` to select a random colorscheme
+
+## Tips and Tricks
+
+### Select a random colorscheme at startup
+
+```lua
+{
+  ‘thomasmarcel/tune.nvim’,
+  config = function()
+    local tune = require 'tune'
+    tune.setup()
+    tune.pick_random_colorscheme()
+  end,
+  priority = 10,
+}
+```
+
+## TODO
+
+- [ ] Pass a list of colorschemes to pick from
+- [ ] Define GUI font family and size
--- lua/tune/init.lua
+++ lua/tune/init.lua
@@ -2,7 +2,7 @@ local M = {}
 
 local function set_default_colorscheme(default_scheme)
 	if default_scheme then
-		vim.cmd('colorscheme ' .. default_scheme)
+		vim.cmd("colorscheme " .. default_scheme)
 	end
 end
 
@@ -10,7 +10,7 @@ function M.setup(config)
 	config = config or {}
 
 	-- Set default colorscheme from configuration or use a default one
-	set_default_colorscheme(config.default_colorscheme or 'rose-pine')
+	set_default_colorscheme(config.default_colorscheme or "rose-pine")
 
 	-- Function to get the list of available colorschemes using the API
 	function M.get_available_colorschemes()
@@ -19,10 +19,10 @@ function M.setup(config)
 		local colorschemes = {}
 
 		for _, path in ipairs(runtime_paths) do
-			local colors_path = path .. '/colors'
-			local colors = vim.fn.glob(colors_path .. '/*.vim', false, true)
+			local colors_path = path .. "/colors"
+			local colors = vim.fn.glob(colors_path .. "/*.vim", false, true)
 			for _, color in ipairs(colors) do
-				local colorscheme = vim.fn.fnamemodify(color, ':t:r')
+				local colorscheme = vim.fn.fnamemodify(color, ":t:r")
 				table.insert(colorschemes, colorscheme)
 			end
 		end
@@ -32,7 +32,7 @@ function M.setup(config)
 
 	-- Function to set a colorscheme
 	function M.set_colorscheme(colorscheme)
-		vim.cmd('colorscheme ' .. colorscheme)
+		vim.cmd("colorscheme " .. vim.trim(colorscheme))
 	end
 
 	-- Function to pick a random colorscheme
@@ -42,6 +42,10 @@ function M.setup(config)
 		local random_colorscheme = colorschemes[random_index]
 		M.set_colorscheme(random_colorscheme)
 	end
+
+	-- Define Neovim commands
+	vim.cmd('command! -nargs=1 Tune lua require("tune").set_colorscheme(<q-args>)')
+	vim.cmd('command! TuneRandom lua require("tune").pick_random_colorscheme()')
 end
 
 return M
